8e472af198b0bcb0e45ada66361b7d5258d41c2d
Julius Bradley <julius.b.bradley@gmail.com>
2017-05-17 17:25:49 -0700

    first commit. visualizer, save locally function, and record function created

---
+++ node_modules/pizzicato/karma.conf.js
@@ -0,0 +1,22 @@
+module.exports = function(config) {
+
+	var cfg = {
+ 
+		browsers: ['Firefox'],
+		frameworks: ['jasmine'],
+		browserNoActivityTimeout: 30000,
+		files: [
+			'distr/Pizzicato.js',
+			'tests/**/*.js',
+			{ 
+				pattern:  'tests/**/*.*',
+				watched:  false,
+				served:   true,
+				included: false 
+			}
+		]
+	};
+
+	config.set(cfg);
+
+};
